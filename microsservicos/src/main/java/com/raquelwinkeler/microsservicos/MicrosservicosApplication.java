bf3f8fa15b6297d10342378dab5864c9e9ce526f
Raquel Souto <kelsouto@gmail.com>
2022-03-17 17:14:26 -0300

    Adicionar projeto

---
+++ microsservicos/src/main/java/com/raquelwinkeler/microsservicos/MicrosservicosApplication.java
@@ -0,0 +1,13 @@
+package com.raquelwinkeler.microsservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicosApplication.class, args);
+	}
+
+}
