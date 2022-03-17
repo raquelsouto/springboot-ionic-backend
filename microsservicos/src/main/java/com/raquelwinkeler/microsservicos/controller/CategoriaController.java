6a96c16df4ac939a94baf6d0611d14b0872eea09
Raquel Souto <kelsouto@gmail.com>
2022-03-17 19:04:21 -0300

    Testar REST

---
+++ microsservicos/src/main/java/com/raquelwinkeler/microsservicos/controller/CategoriaController.java
@@ -0,0 +1,16 @@
+package com.raquelwinkeler.microsservicos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaController {
+	
+	@GetMapping
+	public String listar() {
+		return "REST está funcionando";
+	}
+
+}
