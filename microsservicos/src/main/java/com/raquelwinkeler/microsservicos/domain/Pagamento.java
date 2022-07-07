be251ec546e7b128069ca02f3d2f0120db084c9c
Raquel Souto <kelsouto@gmail.com>
2022-07-06 23:58:33 -0300

    Mapeamento OneToOne entre pagamento e pedido

--- microsservicos/src/main/java/com/raquelwinkeler/microsservicos/domain/Pagamento.java
+++ microsservicos/src/main/java/com/raquelwinkeler/microsservicos/domain/Pagamento.java
@@ -3,16 +3,23 @@ package com.raquelwinkeler.microsservicos.domain;
 import com.raquelwinkeler.microsservicos.domain.enums.EstadoPagamento;
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Pagamento implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
     private Integer id;
     private EstadoPagamento estado;
+
+    @OneToOne
+    @JoinColumn(name="pedido_id")
+    @MapsId
     private Pedido pedido;
 
 }
--- microsservicos/src/main/java/com/raquelwinkeler/microsservicos/domain/Pedido.java
+++ microsservicos/src/main/java/com/raquelwinkeler/microsservicos/domain/Pedido.java
@@ -2,17 +2,24 @@ package com.raquelwinkeler.microsservicos.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class Pedido implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Date instante;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
     private Pagamento pagamento;
     private Cliente cliente;
     private Endereco enderecoDeEntrega;
